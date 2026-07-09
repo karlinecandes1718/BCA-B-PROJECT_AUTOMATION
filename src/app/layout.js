9cc4398c27cc60c23cd6bb01286dc79f691529ba
shruthikasharonvr-oss <shruthikasharonvr@gmail.com>
2026-07-09 10:27:59 +0530

    bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,6 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col bg-[#F5F8FC] text-[#12203A]">
         <AuthProvider>
